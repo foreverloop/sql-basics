401dfa691531886c7791a25a719bcde20988eb26
foreverloop <cjones5709@gmail.com>
2018-01-04 20:44:29 +0000

    Join statements added

--- select_statements.sql
+++ select_statements.sql
@@ -80,3 +80,69 @@ SELECT customer_id,payment_id,amount
 FROM payment
 WHERE amount BETWEEN 8 AND 9;
 
+/*
+returns every payment that has customer_id associated
+WHERE and ORDER BY can also be specified after the ON syntax
+*/
+SELECT customer.customer_id, first_name,last_name,email,amount,payment_date
+FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+/* inner join on staff, payment and customer 
+return customer first name, staff first name
+payment id and amount
+*/
+SELECT customer.first_name customer_first_name,
+staff.staff_id,staff.first_name staff_first_name,
+payment.payment_id,amount,payment.customer_id
+FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+INNER JOIN staff ON payment.staff_id = staff.staff_id;
+
+/* below shows a FULL OUTER JOIN
+it returns all rows, even if there is not a match in one row side of the join
+so it would return all employees and all departments regardless
+may be useful if for example you had to find an employee not assigned
+to a department
+*/
+SELECT employee_name, department_name
+FROM employees
+FULL OUTER JOIN departments ON d.department_id = e.department_id;
+
+/* return all films, and any inventory record for that film
+if there is no inventory record, it will return NULL for that row
+*/
+SELECT film.film_id,film.title,inventory_id
+FROM film
+LEFT JOIN inventory ON inventory.film_id = film.film_id;
+
+/* There is a NATURAL JOIN, which defaults to an inner join
+unless otherwise specified. 
+it tries to match based on common columns 
+(and if the values are different, it will fail) 
+t's results can be unpredictable. so it's usually better to be specific
+*/
+
+/* Cross join creates a cartesean product as a result
+  e.g below a1,a2,a3,b1,b2,b3
+ */
+CREATE TABLE T1 (label CHAR(1) PRIMARY KEY);
+ 
+CREATE TABLE T2 (score INT PRIMARY KEY);
+ 
+INSERT INTO T1 (label)
+VALUES
+ ('A'),
+ ('B');
+ 
+INSERT INTO T2 (score)
+VALUES
+ (1),
+ (2),
+ (3);
+
+SELECT 
+*
+FROM T1
+CROSS JOIN T2;
+
