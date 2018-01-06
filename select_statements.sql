1d5093f25d2141ae8b20da3f4daaf66cb3133ab0
foreverloop <cjones5709@gmail.com>
2018-01-06 12:07:25 +0000

    more sql commands

--- select_statements.sql
+++ select_statements.sql
@@ -209,4 +209,26 @@ INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
 INNER JOIN film ON inventory.film_id = film.film_id
 GROUP BY film.title
 ORDER BY COUNT(rental_id) DESC) 
-TO 'user/documents/customer_rentals.csv' (format CSV);
+TO 'users/documents/customer_rentals.csv' (format CSV);
+
+COPY(SELECT COUNT(film.film_id),category.name FROM film 
+	INNER JOIN film_category ON film.film_id = film_category.film_id 
+	INNER JOIN category ON film_category.category_id = category.category_id 
+	GROUP BY category.name 
+	ORDER BY COUNT(film.film_id) DESC) 
+TO '/users/documents/film_category_by_count.csv' (format CSV);
+
+COPY(SELECT COUNT(film.film_id),actor.first_name,actor.last_name FROM actor 
+	INNER JOIN film_actor ON film_actor.actor_id = actor.actor_id 
+	INNER JOIN film ON film.film_id = film_actor.film_id 
+	GROUP BY actor.first_name,actor.last_name 
+	ORDER BY COUNT(film.film_id) DESC) 
+TO '/users/documents/most_common_actors.csv' (format CSV);
+
+COPY(SELECT city.city,COUNT(address.address_id) FROM customer 
+	INNER JOIN address ON customer.address_id = address.address_id 
+	INNER JOIN city ON address.city_id = city.city_id 
+	GROUP BY city.city 
+	ORDER BY COUNT(address.address_id) DESC LIMIT 5)
+TO '/users/documents/most_common_cities.csv' (format CSV);
+
